e462f57df0adc94c5f51903c2488d6fadaef77f5
Jesse Millar <hellojessemillar@gmail.com>
2016-05-25 16:36:24 -0600

    Adding Circle stuff

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
-# Pasted into Jenkins to build (will eventually be fleshed out to work with a Docker Hub and Amazon AWS)
 
 echo "Stopping running application"
-docker stop pjlink-microservice
-docker rm pjlink-microservice
+ssh $DEPLOY_USERNAME@$DEPLOY_HOST 'docker stop pjlink-microservice'
+ssh $DEPLOY_USERNAME@$DEPLOY_HOST 'docker rm pjlink-microservice'
 
-echo "Building container"
-docker build -t byuoitav/pjlink-microservice .
+echo "Pulling latest version"
+ssh $DEPLOY_USERNAME@$DEPLOY_HOST 'docker pull byuoitav/pjlink-microservice:latest'
 
 echo "Starting the new version"
-docker run -d --restart=always --name pjlink-microservice -p 8005:8005 byuoitav/pjlink-microservice:latest
+ssh $DEPLOY_USERNAME@$DEPLOY_HOST 'docker run -d --restart=always --name pjlink-microservice -p 8005:8005 byuoitav/pjlink-microservice:latest'
+
+echo "Success!"
+
+exit 0
