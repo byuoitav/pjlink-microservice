e684377c5b7ff47169821d3c47ec6da6c948878e
Jesse Millar <hellojessemillar@gmail.com>
2016-06-14 14:53:41 -0600

    Fixing jsonresp naming

--- controllers/command.go
+++ controllers/command.go
@@ -13,12 +13,12 @@ func Raw(context echo.Context) error {
 
 	err := context.Bind(&request)
 	if err != nil {
-		return jsonresp.Create(context, http.StatusBadRequest, "Could not read request body: "+err.Error())
+		return jsonresp.New(context, http.StatusBadRequest, "Could not read request body: "+err.Error())
 	}
 
 	response, err := helpers.HandleRawRequest(request)
 	if err != nil {
-		return jsonresp.Create(context, http.StatusBadRequest, err.Error())
+		return jsonresp.New(context, http.StatusBadRequest, err.Error())
 	}
 
 	return context.JSON(http.StatusOK, response)
