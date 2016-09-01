f1a6a7806f4dcc968edb2bab2a3e55ca44101303
sdanieru <daniel.petersen@byu.edu>
2016-09-01 16:10:21 -0600

    added standard endpoints for av service

--- handlers/command.go
+++ handlers/command.go
@@ -13,7 +13,8 @@ func Command(context echo.Context) error {
 
 	requestError := context.Bind(&request)
 	if requestError != nil {
-		return jsonresp.New(context, http.StatusBadRequest, "Could not read request body: "+requestError.Error())
+		return jsonresp.New(context, http.StatusBadRequest,
+			"Could not read request body: "+requestError.Error())
 	}
 
 	response, responseError := pjlink.HandleRequest(request)
@@ -25,29 +26,7 @@ func Command(context echo.Context) error {
 }
 
 func CommandInfo(context echo.Context) error {
-	return jsonresp.New(context, http.StatusBadRequest, "Send a POST request to the /command endpoint with a body including Address, Port, Class, Password, Command, and Parameter tokens")
-}
-
-func PowerOn(context echo.Context) error {
-	address := context.Param("address")
-
-	request := pjlink.PJRequest{Address: address, Port: "4352", Password: "test", Class: "1", Command: "power", Parameter: "power-on"}
-	response, responseError := pjlink.HandleRequest(request)
-	if responseError != nil {
-		return jsonresp.New(context, http.StatusBadRequest, responseError.Error())
-	}
-
-	return context.JSON(http.StatusOK, response)
-}
-
-func PowerOff(context echo.Context) error {
-	address := context.Param("address")
-	//TODO: Pull projector password out into a variable.
-	request := pjlink.PJRequest{Address: address, Port: "4352", Password: "test", Class: "1", Command: "power", Parameter: "power-off"}
-	response, responseError := pjlink.HandleRequest(request)
-	if responseError != nil {
-		return jsonresp.New(context, http.StatusBadRequest, responseError.Error())
-	}
-
-	return context.JSON(http.StatusOK, response)
+	return jsonresp.New(context, http.StatusBadRequest, "Send a POST request to"+
+		"the /command endpoint with a body including Address, Port, Class,"+
+		"Password, Command, and Parameter tokens")
 }
