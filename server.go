cc20378eebb6ad72b3fc6d41912e59e11d0c6d1b
thailandmatt <31248374+thailandmatt@users.noreply.github.com>
2018-11-05 14:00:58 -0700

    remove echo from vendor, auth changes

--- server.go
+++ server.go
@@ -3,7 +3,6 @@ package main
 import (
 	"net/http"
 
-	"github.com/byuoitav/authmiddleware"
 	"github.com/byuoitav/hateoas"
 	"github.com/byuoitav/pjlink-microservice/handlers"
 	"github.com/jessemillar/health"
@@ -18,32 +17,32 @@ func main() {
 	router.Use(middleware.CORS())
 
 	// Use the `secure` routing group to require authentication
-	secure := router.Group("", echo.WrapMiddleware(authmiddleware.Authenticate))
+	//secure := router.Group("", echo.WrapMiddleware(authmiddleware.Authenticate))
 
 	router.GET("/", echo.WrapHandler(http.HandlerFunc(hateoas.RootResponse)))
 	router.GET("/health", echo.WrapHandler(http.HandlerFunc(health.Check)))
 	router.GET("/status", echo.WrapHandler(http.HandlerFunc(health.Check)))
 
-	secure.GET("/raw", handlers.RawInfo)
-	secure.POST("/raw", handlers.Raw)
-	secure.GET("/command", handlers.CommandInfo)
-	secure.POST("/command", handlers.Command)
+	router.GET("/raw", handlers.RawInfo)
+	router.POST("/raw", handlers.Raw)
+	router.GET("/command", handlers.CommandInfo)
+	router.POST("/command", handlers.Command)
 
 	//status endpoints
-	secure.GET("/:address/power/status", handlers.GetPowerStatus)
-	secure.GET("/:address/display/status", handlers.GetBlankedStatus)
-	secure.GET("/:address/volume/mute/status", handlers.GetMuteStatus)
-	secure.GET("/:address/input/current", handlers.GetCurrentInput)
-	secure.GET("/:address/input/list", handlers.GetInputList)
+	router.GET("/:address/power/status", handlers.GetPowerStatus)
+	router.GET("/:address/display/status", handlers.GetBlankedStatus)
+	router.GET("/:address/volume/mute/status", handlers.GetMuteStatus)
+	router.GET("/:address/input/current", handlers.GetCurrentInput)
+	router.GET("/:address/input/list", handlers.GetInputList)
 
 	//functionality endpoints
-	secure.GET("/:address/power/on", handlers.PowerOn)
-	secure.GET("/:address/power/standby", handlers.PowerOff)
-	secure.GET("/:address/display/blank", handlers.DisplayBlank)
-	secure.GET("/:address/display/unblank", handlers.DisplayUnBlank)
-	secure.GET("/:address/volume/mute", handlers.VolumeMute)
-	secure.GET("/:address/volume/unmute", handlers.VolumeUnMute)
-	secure.GET("/:address/input/:port", handlers.SetInputPort)
+	router.GET("/:address/power/on", handlers.PowerOn)
+	router.GET("/:address/power/standby", handlers.PowerOff)
+	router.GET("/:address/display/blank", handlers.DisplayBlank)
+	router.GET("/:address/display/unblank", handlers.DisplayUnBlank)
+	router.GET("/:address/volume/mute", handlers.VolumeMute)
+	router.GET("/:address/volume/unmute", handlers.VolumeUnMute)
+	router.GET("/:address/input/:port", handlers.SetInputPort)
 
 	server := http.Server{
 		Addr:           port,
