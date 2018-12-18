825b4aa4267e8fcabd1f3bd0d6fb856f3d953c09
jpw547 <jwerner547@gmail.com>
2018-12-18 12:20:14 -0700

    reworking the authentication aspect

--- handlers/command.go
+++ handlers/command.go
@@ -3,21 +3,12 @@ package handlers
 import (
 	"net/http"
 
-	"github.com/byuoitav/common/log"
-	"github.com/byuoitav/common/v2/auth"
 	"github.com/byuoitav/pjlink-microservice/pjlink"
 	"github.com/jessemillar/jsonresp"
 	"github.com/labstack/echo"
 )
 
 func Command(context echo.Context) error {
-	if ok, err := auth.CheckAuthForLocalEndpoints(context, "write-state"); !ok {
-		if err != nil {
-			log.L.Warnf("Problem getting auth: %v", err.Error())
-		}
-		return context.String(http.StatusUnauthorized, "unauthorized")
-	}
-
 	request := pjlink.PJRequest{}
 
 	requestError := context.Bind(&request)
@@ -41,13 +32,6 @@ func Command(context echo.Context) error {
 }
 
 func CommandInfo(context echo.Context) error {
-	if ok, err := auth.CheckAuthForLocalEndpoints(context, "write-state"); !ok {
-		if err != nil {
-			log.L.Warnf("Problem getting auth: %v", err.Error())
-		}
-		return context.String(http.StatusUnauthorized, "unauthorized")
-	}
-
 	jsonresp.New(context.Response(), http.StatusBadRequest, "Send a POST request to the /command endpoint with a body including Address, Port, Class, Password, Command, and Parameter tokens")
 	return nil
 }
--- handlers/raw.go
+++ handlers/raw.go
@@ -3,21 +3,12 @@ package handlers
 import (
 	"net/http"
 
-	"github.com/byuoitav/common/log"
-	"github.com/byuoitav/common/v2/auth"
 	"github.com/byuoitav/pjlink-microservice/pjlink"
 	"github.com/jessemillar/jsonresp"
 	"github.com/labstack/echo"
 )
 
 func Raw(context echo.Context) error {
-	if ok, err := auth.CheckAuthForLocalEndpoints(context, "write-state"); !ok {
-		if err != nil {
-			log.L.Warnf("Problem getting auth: %v", err.Error())
-		}
-		return context.String(http.StatusUnauthorized, "unauthorized")
-	}
-
 	request := pjlink.PJRequest{}
 
 	requestError := context.Bind(&request)
@@ -36,13 +27,6 @@ func Raw(context echo.Context) error {
 }
 
 func RawInfo(context echo.Context) error {
-	if ok, err := auth.CheckAuthForLocalEndpoints(context, "write-state"); !ok {
-		if err != nil {
-			log.L.Warnf("Problem getting auth: %v", err.Error())
-		}
-		return context.String(http.StatusUnauthorized, "unauthorized")
-	}
-
 	jsonresp.New(context.Response(), http.StatusBadRequest, "Send a POST request to the /raw endpoint with a body including Address, Port, Class, Password, Command, and Parameter tokens")
 	return nil
 }
